33545f3c49b9a5e32bdb7ade1d27668a09297199
Logan Leger <logan@loganleger.com>
2012-12-10 12:46:20 -0600

    Using bower to track asset dependencies

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -1,18 +1,18 @@
 require 'jekyll_asset_pipeline'
 
-# module JekyllAssetPipeline
-#   class CoffeeScriptConverter < JekyllAssetPipeline::Converter
-#     require 'coffee-script'
-# 
-#     def self.filetype
-#       '.coffee'
-#     end
-# 
-#     def convert
-#       return CoffeeScript.compile(@content)
-#     end
-#   end
-# end
+module JekyllAssetPipeline
+  class CoffeeScriptConverter < JekyllAssetPipeline::Converter
+    require 'coffee-script'
+
+    def self.filetype
+      '.coffee'
+    end
+
+    def convert
+      return CoffeeScript.compile(@content)
+    end
+  end
+end
 
 module JekyllAssetPipeline
   class SassConverter < JekyllAssetPipeline::Converter
@@ -23,43 +23,34 @@ module JekyllAssetPipeline
     end
 
     def convert
-      begin
-        sass = Sass::Engine.new(@content, syntax: :scss).render
-      rescue Exception => e
-        p "FAIL"
-        p e
-      end
-      return sass
-      # p @content
-      # sass = Sass::Engine.new(@content, syntax: :scss).render
-      # p sass.to_s
-      # return sass
+      return Sass::Engine.new(@content, syntax: :scss,
+                                        load_paths: ['.', './_components/sass-twitter-bootstrap/lib']).render
     end
   end
 end
 
-# module JekyllAssetPipeline
-#   class CssCompressor < JekyllAssetPipeline::Compressor
-#     require 'yui/compressor'
-# 
-#     def self.filetype
-#       '.css'
-#     end
-# 
-#     def compress
-#       return YUI::CssCompressor.new.compress(@content)
-#     end
-#   end
-# 
-#   class JavaScriptCompressor < JekyllAssetPipeline::Compressor
-#     require 'yui/compressor'
-# 
-#     def self.filetype
-#       '.js'
-#     end
-# 
-#     def compress
-#       return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
-#     end
-#   end
-# end
+module JekyllAssetPipeline
+  class CssCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.css'
+    end
+
+    def compress
+      return YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
+  class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.js'
+    end
+
+    def compress
+      return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
+    end
+  end
+end
